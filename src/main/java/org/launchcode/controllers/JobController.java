0e10400b13be91263243e2ed33789640cb33b9e3
Praveen Guruge <praveenguruge@gmail.com>
2017-07-25 03:30:52 -0500

    completed to do's

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -1,5 +1,6 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Job;
 import org.launchcode.models.forms.JobForm;
 import org.launchcode.models.data.JobData;
 import org.springframework.stereotype.Controller;
@@ -23,8 +24,7 @@ public class JobController {
     @RequestMapping(value = "", method = RequestMethod.GET)
     public String index(Model model, int id) {
 
-        // TODO #1 - get the Job with the given ID and pass it into the view
-
+        model.addAttribute(jobData.findById(id));
         return "job-detail";
     }
 
@@ -37,11 +37,14 @@ public class JobController {
     @RequestMapping(value = "add", method = RequestMethod.POST)
     public String add(Model model, @Valid JobForm jobForm, Errors errors) {
 
-        // TODO #6 - Validate the JobForm model, and if valid, create a
-        // new Job and add it to the jobData data store. Then
-        // redirect to the job detail view for the new Job.
+        if(errors.hasErrors()){
+            model.addAttribute(jobForm);
+            return "new-job";
+        }
+        Job job = new Job(jobForm.getName(), JobData.getInstance().getEmployers().findById(jobForm.getEmployerId()), JobData.getInstance().getLocations().findById(jobForm.getLocationId()), JobData.getInstance().getPositionTypes().findById(jobForm.getPositionTypeId()), JobData.getInstance().getCoreCompetencies().findById(jobForm.getCoreCompetencyId()));
+        JobData.getInstance().add(job);
 
-        return "";
+        return "redirect:?id=" + job.getId();
 
     }
 }
